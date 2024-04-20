5b96994d4ece68bdae3c9416fd9bd2dbff5235dd
mannzyuusan <gao37441137@gmail.com>
2024-04-20 19:14:41 +0900

    breezeをインストール、マップでピン指すと住所がわかる

--- resources/views/posts/map.blade.php
+++ resources/views/posts/map.blade.php
@@ -13,53 +13,64 @@
 </head>
 <body>
     <h1>My Google Map</h1>
-    <!-- 検索ボックス -->
-    <input type="text" id="searchBox" placeholder="地名を入力してください">
     <!-- 地図を表示するための div 要素 -->
     <div id="map"></div>
 
     <script>
+        var map;
+
         // Google Maps API を使用して地図を初期化する関数
         function initMap() {
             // 地図を表示する要素を取得
             var mapElement = document.getElementById('map');
-            // 検索ボックスを取得
-            var searchBox = document.getElementById('searchBox');
-            // デフォルトの中心座標とズームレベル
-            var defaultLocation = { lat: -34.397, lng: 150.644 };
-            var defaultZoom = 8;
+            // 東京駅の位置情報
+            var tokyoStation = { lat: 35.681236, lng: 139.767125 };
+            var defaultZoom = 15; // デフォルトのズームレベル
 
             // 地図を作成
-            var map = new google.maps.Map(mapElement, {
-                center: defaultLocation,
+            map = new google.maps.Map(mapElement, {
+                center: tokyoStation, // 東京駅を中心に表示
                 zoom: defaultZoom
             });
 
-            // 検索ボックスを地図に追加
-            map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchBox);
-
-            // Autocompleteオブジェクトを作成
-            var autocomplete = new google.maps.places.Autocomplete(searchBox);
-
-            // ユーザーが場所を選択すると、その場所のジオコーディングが行われます
-            autocomplete.addListener('place_changed', function() {
-                var place = autocomplete.getPlace();
-                if (!place.geometry) {
-                    // ジオコーディングで場所が見つからない場合はエラーを表示
-                    window.alert("No details available for input: '" + place.name + "'");
-                    return;
+            // マップ上でクリックされたときのイベントハンドラを追加
+            map.addListener('click', function(event) {
+                // クリックされた位置の緯度経度を取得
+                var clickedLocation = event.latLng;
+                // ピンが既に存在する場合は削除する
+                if (typeof marker !== 'undefined') {
+                    marker.setMap(null);
                 }
+                // ピンを設定
+                marker = new google.maps.Marker({
+                    position: clickedLocation,
+                    map: map
+                });
+                // 住所を表示する関数を呼び出し
+                displayAddress(clickedLocation);
+            });
+        }
 
-                // 地図の中心を選択された場所に移動
-                map.setCenter(place.geometry.location);
-                map.setZoom(defaultZoom);
+        // 住所を表示する関数
+        function displayAddress(latlng) {
+            // Geocoderオブジェクトを作成
+            var geocoder = new google.maps.Geocoder();
+            // 住所を取得するリクエストを作成
+            geocoder.geocode({'location': latlng}, function(results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    if (results[0]) {
+                        // 取得した住所を表示
+                        document.getElementById('address').innerHTML = results[0].formatted_address;
+                    } else {
+                        console.log('No results found');
+                    }
+                } else {
+                    console.log('Geocoder failed due to: ' + status);
+                }
             });
         }
     </script>
-
-
-    <script src="https://maps.googleapis.com/maps/api/js?key={{ config('services.google_maps.api_key') }}&callback=initMap" async defer></script>
-     <a href='/posts/index'>ホームはこちら</a>
-
+    <!-- 住所を表示する場所 -->
+    <h1 id="address">ここに住所を表示する</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,32 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [PostController::class, 'index']);
-Route::post('/posts',  [PostController::class, 'store']);
-Route::get('/posts/create',  [PostController::class, 'create']);
-Route::get('/posts/map',[PostController::class,'map']);
-Route::get('/posts/index', [PostController::class, 'index']);
-// Route::get('/posts/{post}',  [PostController::class, 'show']);
-// Route::put('/posts/{post}',  [PostController::class, 'update']);
-// Route::delete('/posts/{post}',  [PostController::class, 'delete']);
-Route::get('/posts/{post}/edit',  [PostController::class, 'edit']);
-Route::get('/categories/{category}', [CategoryController::class,'index']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts/{post}',[PostController::class,'show'])->name('posts.show');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::get('/posts/map', [PostController::class, 'map'])->name('posts.map');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::get('/categories/{category}', [CategoryController::class, 'index'])->name('categories.index');
+});
+
+require __DIR__.'/auth.php';
