80f18f38592d97035456e4223b7bc15bcc4be5df
MoriYoshikatsu <moriyosi36@gmail.com>
2024-04-20 20:28:42 +0900

    いったん保存

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,38 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-    Route::get('/posts/{post}',[PostController::class,'show'])->name('posts.show');
-    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-    Route::get('/posts/map', [PostController::class, 'map'])->name('posts.map');
-    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::get('/categories/{category}', [CategoryController::class, 'index'])->name('categories.index');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::post('/posts',  [PostController::class, 'store']);
+Route::get('/posts/create',  [PostController::class, 'create']);
+Route::get('/posts/{post}',  [PostController::class, 'show']);
+Route::put('/posts/{post}',  [PostController::class, 'update']);
+Route::delete('/posts/{post}',  [PostController::class, 'delete']);
+Route::get('/posts/{post}/edit',  [PostController::class, 'edit']);
+Route::get('/categories/{category}', [CategoryController::class,'index']);
+Route::get('/posts/map',[PostController::class,'map']);
 
 require __DIR__.'/auth.php';
