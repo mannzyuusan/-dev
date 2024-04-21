81c24097da825b1086d04c53aac607ab267f5661
mannzyuusan <gao37441137@gmail.com>
2024-04-21 15:38:29 +0900

    修正、マイグレーション関連、db操作

--- database/migrations/2022_11_03_133415_create_categories_table.php
+++ database/migrations/2022_11_03_133415_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('spots', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
            
